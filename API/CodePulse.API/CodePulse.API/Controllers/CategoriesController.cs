0e514d3eb91d52cd9aab7a90477f612f46947350
AbrahamIchaj <abrahamichaj95@gmail.com>
2026-04-17 11:53:41 -0600

    Modelo y Dto category

---
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using System.Runtime.CompilerServices;
+
+namespace CodePulse.API.Controllers
+{
+
+    // https_://localhost:5001/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDBContext dBContext;
+
+        public CategoriesController(ApplicationDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+           // Map DTO to Model
+
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await dBContext.Categories.AddAsync(category);
+            await dBContext.SaveChangesAsync();
+
+
+            //Model to DTo
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+
+        }
+     
+
+    }
+}
